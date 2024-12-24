ca87318fe49eb36aad245336424e4deddc85718d
Sam Sekhon <simransekhon.fs@gmail.com>
2024-12-24 13:56:03 -0500

    remove all functions

--- api/functions/getconfig/index.js
+++
@@ -1,9 +0,0 @@
-export default async function (context, req) {
-    context.res = {
-        status: 200,
-        body: {
-            endpoint: 'hello',
-            key: 'bye'
-        }
-    };
-};
